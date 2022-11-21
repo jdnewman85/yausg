dae6056792b68711c5c809fc9feea63ab5e0ba48
James Newman <jdnewman85@gmail.com>
2022-11-21 10:36:58 -0700

    Attempt to cache, and set default rust version

--- build_wasm.bash
+++ build_wasm.bash
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 cargo build --release --target wasm32-unknown-unknown
-wasm-bindgen --out-dir ./out/ --target web ./target/wasm32-unknown-unknown/release/
-
+wasm-bindgen --out-dir ./out/ --target web ./target/wasm32-unknown-unknown/release/yausg.wasm
+cp index.html ./out/.
+cd out
+python -m http.server 8000
