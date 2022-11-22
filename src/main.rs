645a0faf03e623b4ce193e6c55b359da7815a2ab
James Newman <jdnewman85@gmail.com>
2022-11-21 17:01:44 -0700

    [0.2.0] - Update to bevy to 0.9.0 and bevy_rapier3d to 0.19.0

--- src/main.rs
+++ src/main.rs
@@ -30,7 +30,7 @@ fn setup(
 ) {
     //Camera
     commands
-        .spawn_bundle(Camera3dBundle {
+        .spawn(Camera3dBundle {
             transform: Transform::from_xyz(3.0, 3.0, 10.0).looking_at(Vec3::ZERO, Vec3::Y),
             ..Default::default()
         })
@@ -41,23 +41,20 @@ fn setup(
 
     //Plane
     commands
-        .spawn()
-        .insert(Collider::cuboid(100.0, 0.1, 100.0))
-        .insert_bundle(PbrBundle {
+        .spawn(PbrBundle {
             mesh: meshes.add(Mesh::from(shape::Plane { size: 5.0 })),
             material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
             transform: Transform::from_xyz(0.0, -2.0, 0.0),
             ..default()
-        });
+        }).insert(Collider::cuboid(100.0, 0.1, 100.0));
 
     //Cube
     commands
-        .spawn()
+        .spawn(TransformBundle::from(Transform::from_xyz(0.0, 4.0, 0.0)))
         .insert(RigidBody::Dynamic)
         .insert(Collider::cuboid(0.5, 0.5, 0.5))
         .insert(Restitution::coefficient(0.7))
-        .insert_bundle(TransformBundle::from(Transform::from_xyz(0.0, 4.0, 0.0)))
-        .insert_bundle(PbrBundle {
+        .insert(PbrBundle {
             mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
             material: materials.add(Color::rgb(1.0, 0.0, 0.0).into()),
             ..default()
